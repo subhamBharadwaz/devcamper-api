22b8cefdf321ea69224f076ffd50cc61edae661a
unknown <subhamsbharadwaz@gmail.com>
2021-03-06 20:19:15 +0530

    create Bootcamp model and  mongoose CRUD in controller

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotEnv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const dbConnection = require('./config/db');
 // Load env vars
 dotEnv.config({ path: './config/config.env' });
@@ -9,6 +10,9 @@ dotEnv.config({ path: './config/config.env' });
 dbConnection();
 
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -22,12 +26,14 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // Close server & exit process
   server.close(() => process.exit(1));
 });
