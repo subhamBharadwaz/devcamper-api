c708367675a47176d0d91bfc5e2890a78f5138cd
unknown <subhamsbharadwaz@gmail.com>
2021-03-06 10:49:09 +0530

    create controller and clean the router

---
+++ controllers/bootcamps.js
@@ -0,0 +1,45 @@
+// @desc     Get all bootcamps
+// @route    GET  /api/v1/bootcamps
+// @access   Public
+
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+};
+
+// @desc     Get single bootcamp
+// @route    GET  /api/v1/bootcamps/:id
+// @access   Public
+
+exports.getBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+};
+
+// @desc     create new bootcamps
+// @route     POST  /api/v1/bootcamps
+// @access   Private
+
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+};
+
+// @desc     Update  bootcamps
+// @route     PUT  /api/v1/bootcamps/:id
+// @access   Private
+
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+};
+
+// @desc     Delete bootcamp
+// @route    DELETE  /api/v1/bootcamps/:id
+// @access   Public
+
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+};
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ dotEnv.config({ path: './config/config.env' });
 
 const app = express();
 
+// Routes
+app.use('/api/v1/bootcamps', require('./routes/bootcamps'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
