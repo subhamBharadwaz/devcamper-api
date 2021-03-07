2aaa6af3e38432d68869018ad39644c0729cac91
unknown <subhamsbharadwaz@gmail.com>
2021-03-07 19:49:15 +0530

    getting bootcamps with geocode

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc     Get all bootcamps
@@ -66,7 +67,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 
 // @desc     Delete bootcamp
 // @route    DELETE  /api/v1/bootcamps/:id
-// @access   Public
+// @access   Private
 
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
@@ -76,4 +77,33 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     );
   }
   res.status(200).json({ success: true, data: {} });
+});
+
+// @desc     Get bootcamps within a radius
+// @route    GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access   Private
+
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Calc radius using radius
+  // Divide distance by radius of earth
+  // Earth Radius = 3,963 mi / 6,378 km
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: {
+      $geoWithin: { $centerSphere: [[lng, lat], radius] },
+    },
+  });
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
 });
