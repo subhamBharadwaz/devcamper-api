ee3b472e989b3b50c40e4ee715ec4d4c467971c1
unknown <subhamsbharadwaz@gmail.com>
2021-03-09 10:22:18 +0530

    role authorization

--- routes/courses.js
+++ routes/courses.js
@@ -8,7 +8,7 @@ const {
 } = require('../controllers/courses');
 
 const Course = require('../models/Course');
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 const advancedResults = require('../middleware/advancedResults');
 
@@ -20,11 +20,11 @@ router
     advancedResults(Course, { path: 'bootcamp', select: 'name description' }),
     getCourses
   )
-  .post(protect, addCourse);
+  .post(protect, authorize('publisher', 'admin'), addCourse);
 router
   .route('/:id')
   .get(getCourse)
-  .put(protect, updateCourse)
-  .delete(protect, deleteCourse);
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
