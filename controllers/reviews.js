afc3b643587651ca27b671056d1c9db135b7b30b
unknown <subhamsbharadwaz@gmail.com>
2021-03-10 20:28:59 +0530

    add review to bootcamp

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -43,3 +43,27 @@ exports.getReview = asyncHandler(async (req, res, next) => {
     data: review,
   });
 });
+
+// @desc     Add review
+// @route    POST  /api/v1/bootcamps/:bootcampId/reviews
+// @access   Private
+
+exports.addReview = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+
+  const bootcmap = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcmap) {
+    return next(
+      new ErrorResponse(`No bootcamp with the ID ${req.params.bootcampId}`, 404)
+    );
+  }
+
+  const review = await Review.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: review,
+  });
+});
